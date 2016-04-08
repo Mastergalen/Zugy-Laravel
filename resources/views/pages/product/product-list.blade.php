272908c5871aeb3f8754bb1c32428505f413a2f0
Galen Han <Mastergalen@users.noreply.github.com>
2016-04-08 18:44:45 +0200

    Fix product list CSS

--- resources/views/pages/product/product-list.blade.php
+++ resources/views/pages/product/product-list.blade.php
@@ -36,45 +36,43 @@
 
             <hr>
 
-            <div id="product-list">
-                <div class="row product-list">
-                    @forelse($products as $p)
-                        <div class="item col-sm-4 col-lg-4 col-md-4 col-xs-12">
-                            <div class="panel panel-default">
-                                <div class="panel-body">
-                                    <a href="{!! $p->getUrl() !!}">
-                                        <img src="{!! $p->cover() !!}" class="img-responsive" alt="{!! $p->title !!}">
-                                    </a>
-                                    <h4><a href="{!! $p->getUrl() !!}">{!! $p->title !!}</a></h4>
-                                    <div class="row">
-                                        <div class="col-xs-6">
-                                            <div class="price"><a href="{!! $p->getUrl() !!}">{!! $p->price !!}&euro;</a>
-                                            </div>
-                                        </div>
-                                        <div class="col-xs-6">
-                                            <button class="btn btn-primary btn-sm pull-right btn-add-cart" data-product-id="{!! $p->id !!}"><i class="fa fa-cart-arrow-down"></i></button>
+            <div class="row product-list">
+                @forelse($products as $p)
+                    <div class="item col-sm-4 col-lg-4 col-md-4 col-xs-12">
+                        <div class="panel panel-default">
+                            <div class="panel-body">
+                                <a href="{!! $p->getUrl() !!}">
+                                    <img src="{!! $p->cover() !!}" class="img-responsive" alt="{!! $p->title !!}">
+                                </a>
+                                <h4><a href="{!! $p->getUrl() !!}">{!! $p->title !!}</a></h4>
+                                <div class="row">
+                                    <div class="col-xs-6">
+                                        <div class="price"><a href="{!! $p->getUrl() !!}">{!! $p->price !!}&euro;</a>
                                         </div>
                                     </div>
-
+                                    <div class="col-xs-6">
+                                        <button class="btn btn-primary btn-sm pull-right btn-add-cart" data-product-id="{!! $p->id !!}"><i class="fa fa-cart-arrow-down"></i></button>
+                                    </div>
                                 </div>
+
                             </div>
                         </div>
-                    @empty
-                        <div class="col-md-12">
-                            @if(isset($query))
-                                <div class="alert alert-info">
-                                    {!! trans('product.search.empty', ['query' => $query]) !!}
-                                </div>
-                            @else
-                                <div class="alert alert-info">
-                                    {!! trans('product.category.empty') !!}
-                                </div>
-                            @endif
-                        </div>
-                    @endforelse
-                </div>
-                {!! $products->appends(['sort' => request('sort'), 'direction' => request('direction')])->links() !!}
+                    </div>
+                @empty
+                    <div class="col-md-12">
+                        @if(isset($query))
+                            <div class="alert alert-info">
+                                {!! trans('product.search.empty', ['query' => $query]) !!}
+                            </div>
+                        @else
+                            <div class="alert alert-info">
+                                {!! trans('product.category.empty') !!}
+                            </div>
+                        @endif
+                    </div>
+                @endforelse
             </div>
+            {!! $products->appends(['sort' => request('sort'), 'direction' => request('direction')])->links() !!}
         </div>
         <div class="col-md-3 col-md-pull-9">
             <div class="panel panel-default">
