7350fbd353bc13542a14d9297ded625056eb90a3
Galen Han <Mastergalen@users.noreply.github.com>
2016-04-11 14:28:21 +0200

    Fix Google Analytics tracking ID

--- resources/views/layouts/barebones.blade.php
+++ resources/views/layouts/barebones.blade.php
@@ -13,9 +13,9 @@ $siteName = Config::get('site.siteName');
                 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                             (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
                         m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-                ga('create', 'UA-72027202-1', 'auto');
+                ga('create', 'UA-28926290-61', 'auto');
                 ga('send', 'pageview');
             </script>
         @endif
