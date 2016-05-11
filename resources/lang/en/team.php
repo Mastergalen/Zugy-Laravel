7e46f2e1f8b9a5a650a3f74608fcddbd56622197
Mastergalen <Mastergalen@users.noreply.github.com>
2016-05-11 20:36:58 +0100

    Update team bios

--- resources/lang/en/team.php
+++ resources/lang/en/team.php
@@ -2,10 +2,11 @@
 
 return [
     'bios.andres.position' => 'President/Co-Founder',
-    'bios.andres.text' => '<p>I am 18 years old (almost 19) and was born in Canada but my parents are both Colombian. I lived mostly in Mississauga and Toronto before moving to Milan for school. I started at Università Bocconi last September and am currently majoring in Finance within the International Economics and Finance program. Although Zugy is my focus at the moment I also aspire for a career in the financial services industry.</p>',
+    'bios.andres.text' => '<p>I was born in Canada after my parents moved there from Colombia and grew up mostly in Mississauga before moving to Milan for school. I decided to apply to schools outside of Canada because I wanted to expand my horizon in terms of job prospects and experience a new culture. I started at Università Bocconi in September 2014 and am currently majoring in Finance within the International Economics and Finance program but hoping to transfer to Economics, Management, and Computer Science.</p>
+<p>My ambition for Zugy is that it becomes the sustainable solution for the consumers of the 21st century and that it continues to develop new methods to please its customers.</p>',
     'bios.galen.position' => 'Chief Technology Officer',
     'bios.galen.text' => '<p>I am currently in my second year studying Computer Science at UCL in London. I have wide experience in full stack web development and am passionate in ensuring that Zugy is using the newest technology to bring its customers the best service and user experience possible.</p>',
-    'bios.matthias.position' => 'Chief Financial Officer',
+    'bios.matthias.position' => 'Chief Administrative Officer',
     'bios.matthias.text' => '<p>I\'m from Cologne, Germany and I currently study Economics at Bocconi University in Milan. We have been working together on Zugy since June 2015, with my friends and partners Rafael, Andres, Victor and Leonie. I decided to take the position as CFO in our company, since I was always a passionate about accounting and work in accounting at a non-profit school project in the North of India. Together I think we are a solid team and I am looking forward to the future years we will work together on Zugy!</p>',
     'bios.rafael.position' => 'CEO/Co-Founder',
     'bios.rafael.text' => '<p>I am 19 years old and Portuguese. Throughout my whole student life I always aspired to major in economics on a university abroad. To reach this I moved to an international school in order to learn a variety of languages (French, Spanish) but mainly to be fluent in English. Today I am at Bocconi University in pursuit of my diploma in economics and management.</p>
