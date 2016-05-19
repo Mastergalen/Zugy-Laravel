ee3266ae1f1b8b6195216001e2b5b72c4547d7a3
Mastergalen <Mastergalen@users.noreply.github.com>
2016-05-19 01:24:04 +0100

    Fix double colon

--- resources/views/includes/delivery-time/_closed-warning.blade.php
+++ resources/views/includes/delivery-time/_closed-warning.blade.php
@@ -1,7 +1,7 @@
 @if(!DeliveryTime::isOpen(Carbon::now()))
     <div class="alert alert-warning">
         <p><strong>{!! trans('opening-times.closed') !!}</strong> {!! trans('opening-times.delivery-time') !!}</p>
-        <p><b>{!! trans('opening-times.times') !!}:</b></p>
+        <p><b>{!! trans('opening-times.times') !!}</b></p>
         @include('includes.delivery-time._opening-times')
     </div>
 @endif
